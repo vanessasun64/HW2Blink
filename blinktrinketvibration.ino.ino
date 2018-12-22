54b7f8a966f34b22ecf341067818af43a49d93c7
vanessasun64 <41241474+vanessasun64@users.noreply.github.com>
2018-12-22 15:33:08 -0500

    Add files via upload

---
+++ blinktrinketvibration.ino.ino
@@ -0,0 +1,91 @@
+#define motorPin 0
+
+void setup()
+{
+pinMode(motorPin, OUTPUT);
+}
+
+void loop()
+{
+digitalWrite(motorPin, HIGH);
+delay(500);
+digitalWrite(motorPin, LOW);
+delay (500);
+
+/* digitalWrite(motorPin, HIGH);
+delay(500);
+digitalWrite(motorPin, LOW);
+delay (500);
+
+digitalWrite(motorPin, HIGH);
+delay(500);
+digitalWrite(motorPin, LOW);
+delay (500);
+
+digitalWrite(motorPin, HIGH);
+delay(500);
+digitalWrite(motorPin, LOW);
+delay (500);
+
+digitalWrite(motorPin, HIGH);
+delay(500);
+digitalWrite(motorPin, LOW);
+delay (500);
+
+digitalWrite(motorPin, HIGH);
+delay(500);
+digitalWrite(motorPin, LOW);
+delay (500);
+
+digitalWrite(motorPin, HIGH);
+delay(500);
+digitalWrite(motorPin, LOW);
+delay (500);
+
+digitalWrite(motorPin, HIGH);
+delay(500);
+digitalWrite(motorPin, LOW);
+delay (500);
+
+digitalWrite(motorPin, HIGH);
+delay(500);
+digitalWrite(motorPin, LOW);
+delay (500);
+
+digitalWrite(motorPin, HIGH);
+delay(500);
+digitalWrite(motorPin, LOW);
+delay (500);
+
+digitalWrite(motorPin, HIGH);
+delay(500);
+digitalWrite(motorPin, LOW);
+delay (500);
+
+digitalWrite(motorPin, HIGH);
+delay(500);
+digitalWrite(motorPin, LOW);
+delay (500);
+
+digitalWrite(motorPin, HIGH);
+delay(500);
+digitalWrite(motorPin, LOW);
+delay (500);
+
+digitalWrite(motorPin, HIGH);
+delay(500);
+digitalWrite(motorPin, LOW);
+delay (500);
+
+digitalWrite(motorPin, HIGH);
+delay(500);
+digitalWrite(motorPin, LOW);
+delay (500);
+
+digitalWrite(motorPin, HIGH);
+delay(500);
+digitalWrite(motorPin, LOW);
+delay (500);
+
+delay(50000); */
+}
